d561addf4308d401dc550b48cf10c5c52f0eeba2
official IT Smansaci <ayamsayrrr78@gmail.com>
2026-05-18 10:46:13 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563EB',   // Biru Elektrik
+        secondary: '#F3F4F6', // Abu-abu terang
+        accent: '#FACC15',    // Kuning
+        darkBg: '#111827',    // Background dark mode
+      }
+    },
+  },
+  plugins: [],
+}
